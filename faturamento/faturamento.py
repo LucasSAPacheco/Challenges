d9a5427effe4ad58c24052ebc9d612acdd103a43
LucasSAPacheco <lukssapsilva@gmail.com>
2023-03-10 13:12:25 -0300

    Faturamento

---
+++ faturamento/faturamento.py
@@ -0,0 +1,24 @@
+import json
+
+with open('dados_faturamento.json') as arquivo:
+    dados = json.load(arquivo)
+
+faturamento_diario = dados['faturamento_diario']
+
+soma = 0
+qtd_dias = 0
+for faturamento in faturamento_diario:
+    if faturamento != 0:
+        soma += faturamento
+        qtd_dias += 1
+media = soma / qtd_dias
+
+
+menor = min(faturamento_diario)
+maior = max(faturamento_diario)
+
+acima_media = sum(1 for faturamento in faturamento_diario if faturamento > media)
+
+print(f"Menor faturamento diário: R$ {menor:.2f}")
+print(f"Maior faturamento diário: R$ {maior:.2f}")
+print(f"Dias acima da média: {acima_media}")
