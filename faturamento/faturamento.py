9d9f0d49ecefed79c93d9155ca0dc841d133eaa5
LucasSAPacheco <lukssapsilva@gmail.com>
2023-03-10 13:37:54 -0300

    faturamento mensal

--- faturamento/faturamento.py
+++ faturamento/faturamento.py
@@ -1,24 +1,28 @@
 import json
 
-with open('dados_faturamento.json') as arquivo:
-    dados = json.load(arquivo)
+def faturamento():
+    with open('dados_faturamento.json') as arquivo:
+        dados = json.load(arquivo)
 
-faturamento_diario = dados['faturamento_diario']
+    faturamento_diario = dados['faturamento_diario']
 
-soma = 0
-qtd_dias = 0
-for faturamento in faturamento_diario:
-    if faturamento != 0:
-        soma += faturamento
-        qtd_dias += 1
-media = soma / qtd_dias
+    soma = 0
+    qtd_dias = 0
+    for faturamento in faturamento_diario:
+        if faturamento != 0:
+            soma += faturamento
+            qtd_dias += 1
+    media = soma / qtd_dias
 
 
-menor = min(faturamento_diario)
-maior = max(faturamento_diario)
+    menor = min(faturamento_diario)
+    maior = max(faturamento_diario)
 
-acima_media = sum(1 for faturamento in faturamento_diario if faturamento > media)
+    acima_media = sum(1 for faturamento in faturamento_diario if faturamento > media)
 
-print(f"Menor faturamento diário: R$ {menor:.2f}")
-print(f"Maior faturamento diário: R$ {maior:.2f}")
-print(f"Dias acima da média: {acima_media}")
+    print(f"Menor faturamento diário: R$ {menor:.2f}")
+    print(f"Maior faturamento diário: R$ {maior:.2f}")
+    print(f"Dias acima da média: {acima_media}")
+
+if __name__ == '__main__':
+    faturamento()
---
+++ faturamento_mensal/faturamento_mensal.py
@@ -0,0 +1,18 @@
+def faturamento_mensal():
+    faturamento = {
+        'SP': 67836.43,
+        'RJ': 36678.66,
+        'MG': 29229.88,
+        'ES': 27165.48,
+        'Outros': 19849.53
+    }
+
+    total = sum(faturamento.values())
+    percentuais = {estado: (valor / total) * 100 for estado, valor in faturamento.items()}
+    for estado, percentual in percentuais.items():
+        print(f"{estado}: {percentual:.2f}%")
+    return
+
+
+if __name__ == '__main__':
+    faturamento_mensal()
